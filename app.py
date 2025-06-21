e45297a947b524747f2d23ca8a5b8ff86d985ee9
Muhammad Shoaib <mshoaib3393@gmail.com>
2025-06-21 03:35:34 -0700

    Update app.py

--- app.py
+++ app.py
@@ -61,5 +61,8 @@ def forecast():
                            is_stationary=is_stationary,
                            summary=model_fit.summary().as_text())
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+
+    if __name__ == "__main__":
+       port = int(os.environ.get("PORT", 5000))
+       app.run(debug=False, host='0.0.0.0', port=port)
